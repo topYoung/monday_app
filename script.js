a70b15e15cd9462a8d096e99bba3f85f6655c181
topYoung <103985664+topYoung@users.noreply.github.com>
2024-03-25 12:02:42 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ const headers = {
 
 // const callback = res => console.log("filter_res=",res);
 monday.listen('filter', (res)=>{
-    console.log("filter_res=",res
+    console.log("filter_res=",res)
     monday.get("filter")
     .then(res => console.log("get_filter_update=",res))
 });
