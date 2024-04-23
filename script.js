ce34296c925d0235039329fef984791dddc6d5ab
topYoung <103985664+topYoung@users.noreply.github.com>
2024-04-23 11:49:08 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -174,9 +174,9 @@ function setData(){
                 div.className = 'right_text_box'
                 let t1 = document.createElement('p')
                 t1.innerHTML = checkbox.title
-                t1.className = right_text_content
+                t1.className = 'right_text_content'
                 let t2 = document.createElement('p')
-                t2.className = right_text_content
+                t2.className = 'right_text_content'
                 t2.innerHTML = getValue(checkbox.id,column_values)
                 div.appendChild(t1)
                 div.appendChild(t2)
